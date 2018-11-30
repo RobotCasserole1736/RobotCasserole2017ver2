4f72474a53f5c197c02716e450073dbdbbd3a5e8
BUTTERCUP <buttercup@robotcasserole.com>
2018-11-29 19:53:38 -0600

    Started work on the turret. Created functions for both closed and open loop.

--- src/main/java/frc/robot/Turret.java
+++ src/main/java/frc/robot/Turret.java
@@ -1,15 +1,25 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.XboxController;
 
 public class Turret {
 
     DigitalInput leftLimitSw;
     DigitalInput rightLimitSw;
 
+    Encoder posEncoder;
+
+    XboxController openLoopController;
+
     double openLoopRotationCmd;
     double closedLoopPosCmd_deg;
+    double actualPos;
+    double motorCmd;
+
+    int closedLoopIndex = 0;
 
     boolean runClosedLoop;
 
@@ -19,6 +29,8 @@ public class Turret {
         motorController = new Spark(0);
         leftLimitSw = new DigitalInput(0);
         rightLimitSw = new DigitalInput(1);
+        posEncoder = new Encoder(2, 3);
+        openLoopController = new XboxController(0);
 
         runClosedLoop = false;
 
@@ -29,13 +41,40 @@ public class Turret {
         openLoopRotationCmd = cmd;
     }
 
-    public void setClosedLoopPositionCommand(double pos_deg){
+    public void setClosedLoopPositionCommand(double index){
         runClosedLoop = true;
-        closedLoopPosCmd_deg = pos_deg;
+        if(index == 0){
+            closedLoopPosCmd_deg = 45;
+        }else if(index == 1){
+            closedLoopPosCmd_deg = 90;
+        }else if(index == 2){
+            closedLoopPosCmd_deg = 135;
+        }
+    }
+
+    public void getActualPosition(){
+        posEncoder.setDistancePerPulse((1/1024)*(25/500)*(360)); //Values to be changed when we know exact
+        actualPos = posEncoder.getDistance();
+    }
+
+    public void bangBang(){
+        if((actualPos <= (closedLoopPosCmd_deg + 1)) || (actualPos >= (closedLoopPosCmd_deg - 1)){
+            motorCmd = motorController.set(0);
+        }else if(actualPos <= closedLoopPosCmd_deg){
+            motorCmd = motorController.set(-1);
+        }else if(actualPos >= closedLoopPosCmd_deg){
+            motorCmd = motorController.set(1);
+        }
     }
 
     public void update(){
-        //Todo
+        if(runClosedLoop == false){
+            setOpenLoopRotationCommand(openLoopController.getX(Hand kleft)); //Will fix this later
+        }else if(runClosedLoop == true){
+            setClosedLoopPositionCommand(closedLoopIndex); //This index will need to be set. Currently is 0
+            getActualPosition();
+            bangBang();
+        }  
     }
 
 
