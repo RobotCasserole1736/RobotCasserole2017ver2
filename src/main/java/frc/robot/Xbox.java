8ac50659bc429983dbb4191a0840c58e271f176a
ADA <ada@robotcasserole.org>
2018-12-13 19:38:23 -0600

    commiting math for dt

--- src/main/java/frc/robot/Xbox.java
+++ src/main/java/frc/robot/Xbox.java
@@ -7,26 +7,40 @@ import edu.wpi.first.wpilibj.GenericHID.Hand;
 
 public class Xbox {
 
-    XboxController xboxCtrlr;
+    XboxController xbox;
 
-    double XboxFwdRevCmd = 0;
-    double XboxRotateCmd = 0;
+    double xboxFwdRevCmd = 0;
+    double xboxRotateCmd;
 
     
 private Xbox() {
-    xboxCtrlr = new XboxController(0);
+    xbox = new XboxController(0);
 }
 
 
-public double getX(GenericHID.Hand kRight){
+public double getXboxRotateCmd(){
     
-    
-    return XboxRotateCmd;
+    if (xbox.getX(Hand.kRight) > 0.5) {
+        xboxRotateCmd *= 0.85;
+    if (xbox.getX(Hand.kRight) < 0.15 & xbox.getX(Hand.kRight) > -0.15) {
+        xboxRotateCmd = xbox.getX(Hand.kRight);
+        }
+    }
+    xboxRotateCmd = Math.pow(xboxRotateCmd, 3);
+    return xboxRotateCmd;
 }
+
 public double getY(GenericHID.Hand kLeft){
     
+    if (xbox.getX(Hand.kRight) > 0.5) {
+        xboxFwdRevCmd *= 0.7;
+    if (xbox.getX(Hand.kRight) < 0.15 & xbox.getX(Hand.kRight) > -0.15) {
+        xboxFwdRevCmd = xbox.getX(Hand.kRight);
+        }
+    }
+    xboxFwdRevCmd = Math.pow(xboxRotateCmd, 3);
     
-    return XboxFwdRevCmd;
+    return xboxFwdRevCmd;
 }
 
 
